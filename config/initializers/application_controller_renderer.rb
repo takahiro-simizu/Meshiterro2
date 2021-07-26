f9c3f686d66f3b548066b37d75af63e43c959f32
takahiro-simizu <tkbchando@gmail.com>
2021-07-26 06:59:15 +0000

    [Add]画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f31487b5298fc5231d20c0d9a99fb838b805edf2954e0ce0aa0e411071ebea345a1ecf8236723c2cbaa52268008fbdf1eb6869d963f4917f59e67859c4eca13e'
